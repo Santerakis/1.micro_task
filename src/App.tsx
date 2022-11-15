3c50507f0636046ff3fcb111c62f91a41ffb2322
santerakis <santerakis@gmail.com>
2022-11-15 11:50:49 +0200

    button

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const myFirstSubscriber = () => {
+        console.log('Hello Im Vasja!')
+    }
+    const mySecondSubscriber = () => {
+        console.log('Hello Im John!')
+    }
+
+    return (
+        <div>
+            <div className="App">
+                <button onClick={myFirstSubscriber}>MyYoutubeChanel-1</button>
+            </div>
+
+            <div className="App">
+                <button onClick={mySecondSubscriber}>MyYoutubeChanel-2</button>
+            </div>
+        </div>
+    )
 }
 
 export default App;
