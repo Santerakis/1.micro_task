6a0ea1b8792a9e257b796123d3dff92b7e47dfa7
santerakis <santerakis@gmail.com>
2022-11-15 13:44:37 +0200

    transfer in the button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
-    const myManButton = () => {
-        console.log('Hello first')
-    }
 
     // const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
     //     console.log('Hello Im Vasja!')
@@ -13,18 +9,24 @@ function App() {
     // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
     //     console.log('Hello Im John!')
     // }
-    // const onClickHadnler = (name: string) => {
-    //     console.log(name)
-    // }
+    const onClickHadnler = (name: string) => {
+        console.log(name)
+    }
+    const foo1=()=>{
+        console.log(100200)
+    }
+    const foo2=(num: number)=>{
+        console.log(num)
+    }
 
     return (
         <div className="App">
-                {/*<button onClick={(event)=>{console.log('Hello first')}}>myFirstButton</button>*/}
-                <button onClick={(event)=>{console.log('Hello Man')}}>myManButton</button>
-                {/*<button onClick={(event)=>onClickHadnler("Vasja")}>MyYoutubeChanel-1</button>*/}
-                {/*<button onClick={(event)=>onClickHadnler("John")}>MyYoutubeChanel-2</button>*/}
-                {/*<button onClick={()=>onClickHadnler("some info")}>MyYoutubeChanel-3</button>*/}
-
+            {/*<button onClick={(event)=>{console.log('Hello first')}}>myFirstButton</button>*/}
+            <button onClick={()=>onClickHadnler('Vasja')}>MyYoutubeChanel-1</button>
+            <button onClick={()=>onClickHadnler('John')}>MyYoutubeChanel-2</button>
+            {/*<button onClick={()=>onClickHadnler("some info")}>MyYoutubeChanel-3</button>*/}
+            <button onClick={foo1}>1</button>
+            <button onClick={()=>foo2(100200)}>2</button>
         </div>
     )
 }
