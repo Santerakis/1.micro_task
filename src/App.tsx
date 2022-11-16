fa72c40358914ec5acf0e9bc5d6d95a4cdad0272
santerakis <santerakis@gmail.com>
2022-11-16 13:45:05 +0200

    hook two buttons

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,21 @@ import {Button} from "./Components/Button";
 function App() {
 
     // let a = 1
-    let [a, setA] = useState(8)
+    let [a, setA] = useState(0)
 
     const onClickHandler = () => {
         setA(++a)
         console.log(a)
     }
+    const onClickHandler2=()=>{
+        setA(a=0)
+    }
 
     return (
         <div className="App">
             <h1>{a}</h1>
-            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler}>++number</button>
+            <button onClick={onClickHandler2}>0</button>
         </div>
     )
 }
