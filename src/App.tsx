057b826ac752dcd16518d4d82aba954db625e82e
santerakis <santerakis@gmail.com>
2022-11-16 13:31:06 +0200

    hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, {MouseEvent} from 'react';
+import React, {MouseEvent, useState} from 'react';
 import './App.css';
 import {Button} from "./Components/Button";
 
 function App() {
 
-    let a = 1;
+    // let a = 1
+    let [a, setA] = useState(8)
+
+    const onClickHandler = () => {
+        setA(++a)
+        console.log(a)
+    }
 
     return (
         <div className="App">
             <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
         </div>
     )
 }
