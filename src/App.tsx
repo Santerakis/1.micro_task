cc55a4d094cd5eb11afa70d20766aab69ac84447
santerakis <santerakis@gmail.com>
2022-11-16 11:42:39 +0200

    for hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React, {MouseEvent} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Button} from "./Components/Button";
 
 function App() {
 
-    const Button1Foo = (sub: string, age: number, address: string) => {
-        console.log(sub, age, address)
-    }
-    const Button2Foo = (sub: string, age: number) => {
-        console.log(sub, age)
-    }
-    const Button3Foo = () => {
-        console.log('Im stupid button')
-    }
+    let a = 1;
 
     return (
         <div className="App">
-            <Button name={'YouTubeChanel-1'} callBack={() => Button1Foo('Im Vasya', 21, 'live in Latvia')}/>
-            <Button name={'YouTubeChanel-2'} callBack={() => Button2Foo('Im Ivan', 30)}/>
-            <Button name={'Stupid Button'} callBack={Button3Foo}/>
+            <h1>{a}</h1>
         </div>
     )
 }
